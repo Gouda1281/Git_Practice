cddf0cddb82e8e5430a4028aef10be4ddf43582a
Basavanagouda <Basavanagoudahbh.gmail.com>
2025-11-09 22:10:05 -0500

    branch sum first commit

--- Git_practise/main.c
+++ Git_practise/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
     printf("Hello world!\n");
     printf("adding first line after 1st commit\n");
+    printf("write function for sum\n");
     return 0;
 }
