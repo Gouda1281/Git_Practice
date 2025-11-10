96baa7ada2c9e9060ee75e057b16fe1b7644b1c1
Basavanagouda <Basavanagoudahbh.gmail.com>
2025-11-09 23:28:35 -0500

    wrote diff function

--- Git_practise/main.c
+++ Git_practise/main.c
@@ -9,11 +9,18 @@ int sum(int a, int b)
 
 }
 
+int diff(int a,int b)
+{
+
+    return(a-b);
+}
+
 int main()
 {
     printf("Hello world!\n");
     printf("adding first line after 1st commit\n");
     printf("write function for sum\n");
     printf("sum result : %d",sum(1,2));
+    printf("Diff result : %d",sum(3,2));
     return 0;
 }
